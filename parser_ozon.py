7eebf19e19e1813fc6642f0e018af578da5ea632
Work A <you@example.com>
2024-06-01 08:26:28 +0300

    first commit

---
+++ parser_ozon.py
@@ -0,0 +1,12 @@
+import json
+
+
+import requests
+
+if __name__ == '__main__':
+
+    response = requests.get("https://docs.ozon.ru/api/seller/swagger.json")
+    text = response.text
+    api_json = json.loads(text)
+
+    pass
---
+++ parser_wb.py
@@ -0,0 +1,16 @@
+import json
+
+
+import requests
+
+if __name__ == '__main__':
+
+    response = requests.get("https://openapi.wildberries.ru/content/api/ru/")
+    text = response.text
+
+    redoc_state = text[text.find("__redoc_state = ") + 16:]
+    redoc_state = redoc_state[0:redoc_state.find(";\n")]
+
+    api_json = json.loads(redoc_state)
+
+    pass
